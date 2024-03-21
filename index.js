5d81fba591ed40b58ec4375a3f1b1e55d5f69fd5
Gustavo Mendes <gustavormendess@gmail.com>
2024-03-21 16:38:04 -0400

    button onclick

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const elementosDuvida = document.querySelectorAll('.duvida')
-
+const botao = document.querySelector('.botao')
 elementosDuvida.forEach(function(duvida){
   duvida.addEventListener('click',function (){
     duvida.classList.toggle('ativa')
   })
+})
+
+botao.addEventListener('click', function (){
+  alert('Vamos te mandar para uma conversa com a nossa secretária para você agendar um dia!')
 })
