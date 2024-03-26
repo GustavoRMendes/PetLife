c9a9c3404495a354412ba62bb3603b4b187095dc
Gustavo Mendes <gustavormendess@gmail.com>
2024-03-25 23:24:42 -0400

    confirm javascript

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
-const elementosDuvida = document.querySelectorAll('.duvida')
-const botao = document.querySelector('.botao')
-elementosDuvida.forEach(function(duvida){
-  duvida.addEventListener('click',function (){
-    duvida.classList.toggle('ativa')
-  })
-})
+const elementosDuvida = document.querySelectorAll(".duvida");
+const botao = document.querySelector(".botao");
+elementosDuvida.forEach(function (duvida) {
+  duvida.addEventListener("click", function () {
+    duvida.classList.toggle("ativa");
+  });
+});
 
-botao.addEventListener('click', function (){
-  alert('Vamos te mandar para uma conversa com a nossa secretária para você agendar um dia!')
-})
+botao.addEventListener("click", function () {
+  let confirmation = confirm("Confirma sua escolha?");
+  if (confirmation) {
+    alert(
+      "Vamos te mandar para uma conversa com a nossa secretária para você agendar um dia!"
+    );
+  } 
+});
